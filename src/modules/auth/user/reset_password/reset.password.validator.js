68894ac71c1fb6134f42702a0a2b9406e6be565d
beshoybe <beshoy@sitech.me>
2025-03-14 13:53:07 +0200

    update pass reset validation

--- src/modules/auth/user/reset_password/reset.password.validator.js
+++ src/modules/auth/user/reset_password/reset.password.validator.js
@@ -10,18 +10,10 @@ const resetPasswordValidator = z.object({
         .nonempty({message: "validation.password.required"}) // Translation key
         .min(6, {message: "validation.password.min"}) // Translation key
         .max(100, {message: "validation.password.max"}), // Translation key
-    email: z
+        token: z
         .string({
-            required_error: "validation.email.required",
-        })
-        .nonempty({message: "validation.email.required"}) // Translation key
-        .email({message: "validation.email.invalid"}) // Translation key
-        ,
-        otp : z
-        .string({
-            required_error: "validation.otp.required",
-        })
-        .nonempty({message: "validation.otp.required"}) // Translation key  
+            required_error: "validation.token.required",
+        }).nonempty({message: "validation.token.required"}) // Translation key
 
     });
 
