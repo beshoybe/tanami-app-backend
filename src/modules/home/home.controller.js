13e601ec3eeebdcb5429d12332310a16aeb2e07d
beshoybe <beshoy@sitech.me>
2025-03-15 03:01:54 +0200

    update ar select

--- src/modules/home/home.controller.js
+++ src/modules/home/home.controller.js
@@ -3,18 +3,20 @@ const require = createRequire(import.meta.url);
 
 const userInvestments = require("../../../DB/fake_user_investments.json"); // Load JSON
 const investmentData = require("../../../DB/fake_investments.json"); // Load JSON
+const arabicInvestmentData = require("../../../DB/fake_investments_arabic.json"); // Load JSON
 
 const homeController = async (req, res) => {
     try {
-        const { user } = req;
+        const { user,headers } = req;
         const { id, name, email } = user; // Extract only necessary fields
+        const lang = headers["accept-language"];
 
         return res.status(200).json({ 
             message: "Welcome to the home page", 
             data: { 
                 user: { id, name, email }, // Keep user data
                 user_investments: userInvestments,  // Include investment data
-                best_investments: investmentData.investments.slice(0, 2), // Include first two investment data
+                best_investments: lang === "ar" ? arabicInvestmentData.investments.slice(0, 2) : investmentData.investments.slice(0, 2), // Include first two investment data
             } 
         });
     } catch (error) {
