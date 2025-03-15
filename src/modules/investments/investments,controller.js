932c572b63e57285de0d60e05ae56754906c99da
beshoybe <beshoy@sitech.me>
2025-03-15 02:57:49 +0200

    update ar investments

--- src/modules/investments/investments,controller.js
+++ src/modules/investments/investments,controller.js
@@ -1,14 +1,17 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const investmentData = require("../../../DB/fake_investments.json"); // Load JSON
+const arabicInvestmentData = require("../../../DB/fake_investments_arabic.json"); // Load JSON
 
 
 const investmentsController = async (req, res) => {
     try {
+        const {headers} = req;
+        const lang = headers["accept-language"];
         return res.status(200).json({
             message: "Investments data",
             data: {
-                investments: investmentData.investments,
+                investments: lang === "ar" ? arabicInvestmentData.investments : investmentData.investments,
             },
         });
     } catch (error) {
